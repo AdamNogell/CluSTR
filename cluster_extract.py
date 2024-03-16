d9e5531e51b879601578f32f204c832be85da34b
Adam Nógell <adamn.@MacBook-Pro-uzivatela-Adam.local>
2024-03-16 17:14:28 +0100

    output fasta now contains cluster ID instead of the sequence ID + it is sorted

--- cluster_extract.py
+++ cluster_extract.py
@@ -8,20 +8,22 @@ centroid_ids = {}
 with open(sys.argv[1], "r") as f:
     for line in f:
         if line.startswith(">Cluster"):
-            continue  # Skip cluster header lines
+            current_cluster_id = line.strip().split()[-1]
+            continue
         if "*" in line:
             centroid_id = line.strip().split()[2].rstrip("...").lstrip(">")
-            centroid_ids[centroid_id] = centroid_id
+            centroid_ids[centroid_id] = current_cluster_id
 
 # Extract centroid sequences from input FASTA file
 output_sequences = {}
 with open(sys.argv[2], "r") as fasta_file:
     for record in SeqIO.parse(fasta_file, "fasta"):
-        fasta_id = record.id.split()[0]  # Remove any additional characters after the ID
+        fasta_id = record.id.split()[0]
         if fasta_id in centroid_ids:
-            output_sequences[fasta_id] = record.seq
+            output_sequences[centroid_ids[fasta_id]] = record.seq
 
 # Write extracted sequences to a new FASTA file
 with open(sys.argv[3], "w") as extracted:
-    for seq_id, seq in output_sequences.items():
-        extracted.write(f">{seq_id}\n{seq}\n")
+    sorted_sequences = sorted(output_sequences.items(), key=lambda x: int(x[0]))
+    for cluster_id, seq in sorted_sequences:
+        extracted.write(f">{cluster_id}\n{seq}\n")
